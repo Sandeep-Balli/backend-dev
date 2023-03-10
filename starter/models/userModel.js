1c061cad5fdbba575d8c026e70d45602d0f96fdc
Sandeep-Balli <sandeepballi.tech@gmail.com>
2023-03-10 22:05:30 +0530

    Error while setting up advanced features for postman

--- starter/models/userModel.js
+++ starter/models/userModel.js
@@ -34,7 +34,8 @@ const userSchema = new mongoose.Schema({
             },
             message: 'Passwords are not the same'
         }
-    }
+    },
+    passwordChangedAt: Date
 });
 
 userSchema.pre('save', async function(next) {
@@ -53,6 +54,15 @@ userSchema.pre('save', async function(next) {
 
 userSchema.methods.correctPassword = async function(candidatePassword, userPassword) {
     return bcrypt.compare(candidatePassword, userPassword)
+}
+
+userSchema.methods.changedPasswordAfter = function(JWTTimestamp) {
+    if(this.passwordChangedAt) {
+        const changedTimeStamp = parseInt(this.passwordChangedAt.getTime() / 1000, 10);
+        return JWTTimestamp < changedTimeStamp;
+    }
+
+    return false;
 }
 
 const User = mongoose.model('User', userSchema);
